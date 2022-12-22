cf042fe6a1454aebcbe0811bd97376092a8c58ea
satyamannawork <35631089+satyamannawork@users.noreply.github.com>
2022-12-22 13:04:13 +0530

    gate

---
+++ src/main/java/com/ecommerce/ecommercegateway/EcommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommercegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGatewayApplication.class, args);
+	}
+
+}
